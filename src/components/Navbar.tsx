e948e15a2f12b78cb91852d50f8502bd92af3d1d
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-03-17 14:59:33 +0200

    project setup and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+function Navbar() {
+  const [nav, setNav] = useState(false);
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl bold text-[#00df9a]'>DATA.</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+
+      </ul>
+      <div onClick={() => setNav(!nav)} className='block md:hidden'>
+        {nav ? <AiOutlineClose className='h-9 w-9 cursor-pointer' size={20} /> : <AiOutlineMenu className='h-9 w-9 cursor-pointer' size={20} />}
+      </div>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>DATA.</h1>
+        <li className='p-4 border-b border-gray-600'>Home</li>
+        <li className='p-4 border-b border-gray-600'>Company</li>
+        <li className='p-4 border-b border-gray-600'>Resources</li>
+        <li className='p-4 border-b border-gray-600'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
