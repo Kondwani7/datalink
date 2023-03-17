73bf4b011b2f5b31c08b0d65d72d9c368bc74714
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-03-17 16:25:21 +0200

    hero section and start file for analytics page

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Analytics() {
   return (
-    <div>
-        Analytics
+    <div className='w-full bg-white py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+          <Image src="/img/laptop.jpg" className="w-[500px] mx-auto my-4" width={100} height={100} alt=""/>
+        </div>
     </div>
   )
 }
